1da085d6bcb9b0c2ecf9572d8f583d78f3a2061f
Austin Mroczek <austin@mroczek.org>
2019-07-20 13:56:38 -0700

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -6,4 +6,7 @@ chmod 700 ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAk5qQWUPKfPlZoskXAHD1Yl0iSOCfavAoGeCYmVzer1oBgSHsP8B6rgiujHqJWu0kJMKk1xMjUGj04X5bO6FAWqEdoggMlx2M0eb8hF2CQafSzlcWXj3Vi4YtVdv+nLZn+3BFXz0FdCgeXfDlvVbazI6BES2ByAB3tzgWIijz6eDEGwdQB/MjIvSlMtAIKaBCTSNzKLBEXpp7zXN2HJyfTR6YDGEqfKHHOkCTtFDfJyc7NMnw/NQhUKFyrUZnKFTVfWy06NaDXJ12pFWCCA0U3iBUS22shqVZdhYP7xnMgAX/wrwRBgD/SNnUabpqIv/0D1t/ymaYPYCGXq7DT48nJw== rsa-key-20190124" > ~/.ssh/authorized_keys
 chmod 700 ~/.ssh/authorized_keys
 
+# install useful stuff
+apt update
+apt install git
 
