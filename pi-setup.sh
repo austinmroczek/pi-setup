47fe012f28f7ad018de9e8c0d0689eba4b0d5494
austinmroczek <austin@mroczek.org>
2019-12-10 05:08:01 +0000

    Fixed path to authorized_keys

--- pi-setup.sh
+++ pi-setup.sh
@@ -3,7 +3,7 @@
 # setup SSH key
 mkdir ~pi/.ssh
 chmod 700 ~pi/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAk5qQWUPKfPlZoskXAHD1Yl0iSOCfavAoGeCYmVzer1oBgSHsP8B6rgiujHqJWu0kJMKk1xMjUGj04X5bO6FAWqEdoggMlx2M0eb8hF2CQafSzlcWXj3Vi4YtVdv+nLZn+3BFXz0FdCgeXfDlvVbazI6BES2ByAB3tzgWIijz6eDEGwdQB/MjIvSlMtAIKaBCTSNzKLBEXpp7zXN2HJyfTR6YDGEqfKHHOkCTtFDfJyc7NMnw/NQhUKFyrUZnKFTVfWy06NaDXJ12pFWCCA0U3iBUS22shqVZdhYP7xnMgAX/wrwRBgD/SNnUabpqIv/0D1t/ymaYPYCGXq7DT48nJw== rsa-key-20190124" > ~/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAk5qQWUPKfPlZoskXAHD1Yl0iSOCfavAoGeCYmVzer1oBgSHsP8B6rgiujHqJWu0kJMKk1xMjUGj04X5bO6FAWqEdoggMlx2M0eb8hF2CQafSzlcWXj3Vi4YtVdv+nLZn+3BFXz0FdCgeXfDlvVbazI6BES2ByAB3tzgWIijz6eDEGwdQB/MjIvSlMtAIKaBCTSNzKLBEXpp7zXN2HJyfTR6YDGEqfKHHOkCTtFDfJyc7NMnw/NQhUKFyrUZnKFTVfWy06NaDXJ12pFWCCA0U3iBUS22shqVZdhYP7xnMgAX/wrwRBgD/SNnUabpqIv/0D1t/ymaYPYCGXq7DT48nJw== rsa-key-20190124" > ~pi/.ssh/authorized_keys
 chmod 700 ~pi/.ssh/authorized_keys
 chown -R pi:pi ~pi/.ssh
 
